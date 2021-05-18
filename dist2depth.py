fb01cd4cef91802f752974c23217890be11701cc
VictorSchonk <victorschonk@hotmail.com>
2021-05-18 15:55:08 +0200

    cleaned up and added multi face functionality

--- dist2depth.py
+++ dist2depth.py
@@ -12,9 +12,25 @@ def dist2depth(dist):
 	Z = bl*foc/dist
 	return Z
 
-# if faces variable is parced
+# if faces variables are parced (two lists of rectangels (x,y,w,h) of all detected faces)
 def faces2depth(fac1, fac2):
-	if np.size(fac1,0) != np.size(fac2,0):
+	dir = 0 # the direction in which the arrays are filled with faces | https://numpy.org/doc/stable/reference/generated/numpy.ma.size.html
+	if np.size(fac1,dir) != np.size(fac2,dir):
 		print("An error occured with detecting faces")
 	else:
 		# Calculate the center of the rectangles and asses the distance to them
+		x1 = []
+		x2 = []
+		d = []
+		D = []
+		for i in range(np.size(fac1,dir)):
+			x1[i] = fac1[i][0]+fac1[i][2]/2
+			x2[i] = fac2[i][0]+fac2[i][2]/2
+			d[i] = x1[i] - x2[i]
+			D[i] = dist2depth(d[i])
+		'''	
+		for (x,y,w,h) in fac1:
+			x1 = x+w/2
+		for (x,y,w,h) in fac2:
+			x2 = x+w/2
+		'''
