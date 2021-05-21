df07344cf57fdee833d71e5a09b1d3ffc94829d2
VictorSchonk <victorschonk@hotmail.com>
2021-05-21 11:53:22 +0200

    addition of documentation and disparity map function

--- detect.py
+++ detect.py
@@ -1,16 +1,26 @@
-# https://realpython.com/face-recognition-with-python/
+"""
+Document to do facial detection within images.
+
+https://realpython.com/face-recognition-with-python/
+"""
 import cv2
 
-debug = True
+debug = True # Variable to easily turn on or off debug information
 
 def detectFace(image):
+	"""
+	From an input image the face and eye locations are computed. These are returned as 2 variables (faces,eyes).
+
+	IN:		A colour image to extract face and eye locations from
+	OUT:	Faces and Eyes locations in the image
+	"""
 	cascFace = "..\cascade.xml" # Path to the cascade which is the basis of the face detection | haarcascade_frontalface_default | https://github.com/opencv/opencv/tree/master/data/haarcascades
 	faceCascade = cv2.CascadeClassifier(cascFace)
 	cascEye = "..\cascadeEye.xml"
 	eyeCascade = cv2.CascadeClassifier(cascEye)
 
 	#image = cv2.imread(imgPath) # in case the parsed image is not read yet
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert colour image to greyscale
 	#gray = cv2.equalizeHist(gray) # equalize the histogram of the gray image
 
 	faces = faceCascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5, minSize=(40, 40)) #, flags = cv2.CV_HAAR_SCALE_IMAGE)	# Face detection template to use
@@ -18,10 +28,9 @@ def detectFace(image):
 
 	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
-		if debug:cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+		if debug:cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2) # Draw rechtangle around face
 		### DETECT EYES ###
-		faceGray = gray[y:y+h,x:x+w]
+		faceGray = gray[y:y+h,x:x+w] # Determine ROI to detect eyes within a face box
 		eyes = eyeCascade.detectMultiScale(faceGray, scaleFactor=1.1, minNeighbors=5, minSize=(20, 20))
 		if debug:
 			for (x2,y2,w2,h2) in eyes:
--- dist2depth.py
+++ dist2depth.py
@@ -1,26 +1,32 @@
-# Fucntion to calculate the depth of an object from the distance between the centers of object detection based boxes
-#	Based on stereo vision
-#
-#	https://www.geeksforgeeks.org/numpy-size-function-python/
-# Stereo calc:
-#	https://ijirae.com/volumes/Vol4/iss11/05.NVAE10087.pdf
-#	https://www.researchgate.net/profile/Manaf-Zivingy/publication/305308988_Object_distance_measurement_by_stereo_vision/links/5788c12d08aeef933e1b9b35/Object-distance-measurement-by-stereo-vision.pdf
-# OpenCV Stereo doc:
-#	https://docs.opencv.org/master/dd/d53/tutorial_py_depthmap.html 
+"""
+Functions to calculate the depth of objects in stereoscopic images
+	Based on stereo vision
+
+	https://www.geeksforgeeks.org/numpy-size-function-python/
+Stereo calc:
+	https://ijirae.com/volumes/Vol4/iss11/05.NVAE10087.pdf
+	https://www.researchgate.net/profile/Manaf-Zivingy/publication/305308988_Object_distance_measurement_by_stereo_vision/links/5788c12d08aeef933e1b9b35/Object-distance-measurement-by-stereo-vision.pdf
+OpenCV Stereo doc:
+	https://docs.opencv.org/master/dd/d53/tutorial_py_depthmap.html 
+"""
 
 import numpy as np
-#import cv2
+import cv2
 
 bl = None # distance between the 2 camera's
 foc = None # Focal distance of the 2 camera's
 
-# if distance between faces is computed beforehand
 def dist2depth(dist):
+	"""Convert a disparity into a distance/depth"""
 	Z = bl*foc/dist
 	return Z
 
-# if faces variables are parced (two lists of rectangels (x,y,w,h) of all detected faces)
-def faces2depth(fac1, fac2):
+def faces2depth(fac1: list, fac2: list):
+	"""Convert two lists of face locations in images from stereo cameras to the distance they are away from the cameras
+
+	IN:		two lists of rectangels (x,y,w,h) of all detected faces)
+	OUT: 	List of depth
+	"""
 	dir = 0 # the direction in which the arrays are filled with faces | https://numpy.org/doc/stable/reference/generated/numpy.ma.size.html
 	if np.size(fac1,dir) != np.size(fac2,dir):
 		print("An error occured with detecting faces")
@@ -41,3 +47,12 @@ def faces2depth(fac1, fac2):
 		for (x,y,w,h) in fac2:
 			x2 = x+w/2
 		'''
+
+def disp(imL: list, imR: list):
+	"""A function to convert 2 grayscale images into a disparity array.
+	
+	IN:		imageLeft, imageRight (both as greyscale images)
+	OUT:	disparityMap"""
+	stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+	disparity = stereo.compute(imL,imR)
+	return disparity
